e4b4d86f69a05c0303195700acd73a43125d99f4
Jonathan <jonathan.sendiko@gmail.com>
2021-03-25 15:56:30 +0700

    Mengubah objek recruiter dengan data diri sendiri

--- JWork.java
+++ JWork.java
@@ -23,7 +23,7 @@ public class JWork {
      */
     public static void main(String[] args) {
         Location location1 = new Location("Banten", "Tangerang", "Tempat Lahir");
-        Recruiter recruiter = new Recruiter(20, "Matthew", "matthew@gmail.com", "085945644450", location1);
+        Recruiter recruiter = new Recruiter(20, "Jonathan", "jonathan.sendiko@gmail.com", "085945644450", location1);
         Job job = new Job(101, "Web Developer", recruiter, 100000000, "Tech");
         Jobseeker jobseeker = new Jobseeker(1, "Jonathan", "jonathan@cornlearning", "jonathanTekkom", "18 Maret 2021");
         Invoice invoice = new Invoice(102, 2, "18 Maret 2021", 2, jobseeker);
