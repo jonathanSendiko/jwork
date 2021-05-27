628a718910238f2cc46418feb41b93b7b7af6fe5
Jonathan <jonathan.sendiko@gmail.com>
2021-05-27 18:21:04 +0700

    Modul 9 - Case Study - JWork

--- src/main/java/jonathan/jwork/JWork.java
+++ src/main/java/jonathan/jwork/JWork.java
@@ -9,21 +9,21 @@ public class JWork {
 
 
         public static void main(String[] args) {
-                Location location1 = new Location("Banten", "Tangerang", "Tempat Tinggal");
-                Location location2 = new Location("DKI Jakarta", "Jakarta", "Kampus");
-                Location location3 = new Location("Jawa Barat", "Bandung", "Kantor");
-                DatabaseRecruiter.addRecruiter(new Recruiter(DatabaseRecruiter.getLastId() + 1, "Jonathan", "jonathan.sendiko@gmail.com", "085945644450",location1));
-                DatabaseRecruiter.addRecruiter(new Recruiter(DatabaseRecruiter.getLastId() + 1, "Jona", "jonathan.sendiko@hotmail.com", "085945644450",location2));
-                DatabaseRecruiter.addRecruiter(new Recruiter(DatabaseRecruiter.getLastId() + 1, "Jonas", "jonathan.sendiko@ymail.com", "085945644450",location3));
+                Location location1 = new Location("DKI Jakarta", "Jakarta Pusat", "Tanah Abang");
+                Location location2 = new Location("Jawa Barat", "Bandung Barat", "Asia Afrika");
+                Location location3 = new Location("Jawa Timur", "Surabaya Timur", "Jalan jalan");
+                DatabaseRecruiter.addRecruiter(new Recruiter(1, "Jona", "jona@example.com", "123123123", location1));
+                DatabaseRecruiter.addRecruiter(new Recruiter(2, "Jonathan", "jonathan@example.com", "511343235", location2));
+                DatabaseRecruiter.addRecruiter(new Recruiter(3, "Jonathanss", "jonathanss@example.com", "123126163123", location3));
                 try {
-                        DatabaseJob.addJob(new Job(DatabaseJob.getLastId()+1,"Backend Engineer",DatabaseRecruiter.getRecruiterById(1),100000,JobCategory.BackEnd));
-                        DatabaseJob.addJob(new Job(DatabaseJob.getLastId()+1,"Frontend Engineer",DatabaseRecruiter.getRecruiterById(1),100000,JobCategory.FrontEnd));
-                        DatabaseJob.addJob(new Job(DatabaseJob.getLastId()+1,"Data Analyst Tokped",DatabaseRecruiter.getRecruiterById(2),100000,JobCategory.DataAnalyst));
-                        DatabaseJob.addJob(new Job(DatabaseJob.getLastId()+1,"Data Analyst Shopee",DatabaseRecruiter.getRecruiterById(3),100000,JobCategory.DataAnalyst));
-                } catch (RecruiterNotFoundException e) {
-                        e.getMessage();
+                        DatabaseJob.addJob(new Job(1, "Backend Engineer Tokopedia", DatabaseRecruiter.getRecruiterById(1), 10000, JobCategory.BackEnd));
+                        DatabaseJob.addJob(new Job(2, "Frontend Engineer BukaLapak", DatabaseRecruiter.getRecruiterById(2), 10000, JobCategory.FrontEnd));
+                        DatabaseJob.addJob(new Job(3, "UI Engineer Tokopedia", DatabaseRecruiter.getRecruiterById(2), 10000, JobCategory.UI));
+                        DatabaseJob.addJob(new Job(4, "UI Engineer Gojek", DatabaseRecruiter.getRecruiterById(3), 10000, JobCategory.UI));
                 }
-
+                catch(RecruiterNotFoundException e){
+                        e.printStackTrace();
+                }
                 SpringApplication.run(JWork.class, args);
         }
 
